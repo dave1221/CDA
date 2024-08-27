566420832aa664c803c6916d2eff1b1d618e9007
zhujiang028 <zhujiang028@ke.com>
2024-08-28 00:53:46 +0800

    adding initial structure

---
+++ hello.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+"""
+Created on 2024/8/27 22:14
+@author: Zhu Jiang
+"""
+
+
+def add(x, y):
+    return x + y
+
+
+result = add(1, 2)
+print(f"this is the sum of 1, 2, {result}")
